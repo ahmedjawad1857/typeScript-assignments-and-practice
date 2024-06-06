ae3eac111d97f906b47ef5dfc72a9990984a9fd2
Ahmad Jawad <134083952+ahmedjawad1857@users.noreply.github.com>
2024-06-06 18:21:43 +0500

    how-to-set-typeScript

---
+++ howToSetTypeScript/index.js
@@ -0,0 +1,11 @@
+// 1. Write a ts program to find maximum between two numbers.
+var num1 = prompt("enter a number");
+var num2 = prompt("enter a second number");
+num1 > num2
+    ? console.log(num1, "is greater than", num2)
+    : console.log(num2, "is greater than", num1);
+// var num3: any = prompt(`enter first number`);
+// var num4: any = prompt(`enter second number`);
+// var num5: any = prompt(`enter third number`);
+// num3 > num4 && num3 > num5
+//     ? console.log(num3, `is greater than`, num4, num5)
---
+++ howToSetTypeScript/node.js
@@ -0,0 +1,110 @@
+"use strict";
+// // // var Ahmad: String = "Ahmad Jawad";
+// // // console.log(Ahmad);
+// // let amount: any = prompt("Enter the amount:");
+// // let notesCount: any = 0;
+// // if (amount >= 2000) {
+// //   notesCount += Math.floor(amount / 2000);
+// //   amount %= 2000;
+// // }
+// // if (amount >= 500) {
+// //   notesCount += Math.floor(amount / 500);
+// //   amount %= 500;
+// // }
+// // if (amount >= 200) {
+// //   notesCount += Math.floor(amount / 200);
+// //   amount %= 200;
+// // }
+// // if (amount >= 100) {
+// //   notesCount += Math.floor(amount / 100);
+// //   amount %= 100;
+// // }
+// // if (amount >= 50) {
+// //   notesCount += Math.floor(amount / 50);
+// //   amount %= 50;
+// // }
+// // if (amount >= 20) {
+// //   notesCount += Math.floor(amount / 20);
+// //   amount %= 20;
+// // }
+// // if (amount >= 10) {
+// //   notesCount += Math.floor(amount / 10);
+// //   amount %= 10;
+// // }
+// // if (amount >= 5) {
+// //   notesCount += Math.floor(amount / 5);
+// //   amount %= 5;
+// // }
+// // if (amount >= 1) {
+// //   notesCount += Math.floor(amount / 1);
+// //   amount %= 1;
+// // }
+// // console.log("Total number of notes:", notesCount);
+// var marks: any = prompt(`enter obtained marks of chemistry`);
+// var marks: any = prompt(`enter obtained marks of biology`);
+// var marks: any = prompt(`enter obtained marks of mathematics`);
+// var marks: any = prompt(`enter obtained marks of physics`);
+// var marks: any = prompt(`enter obtained marks of computer`);
+// var percentage = (marks / 100) * 100;
+// if (percentage >= 90) {
+//   console.log(`your percentage is`, percentage, ` and your grade is A`);
+// } else if (percentage >= 80) {
+//   console.log(`your percentage is`, percentage, ` and your grade is B`);
+// } else if (percentage >= 70) {
+//   console.log(`your percentage is`, percentage, ` and your grade is C`);
+// } else if (percentage >= 60) {
+//   console.log(`your percentage is`, percentage, `+ and your grade is D`);
+// } else if (percentage >= 40) {
+//   console.log(`your percentage is `, percentage, ` and your grade is E`);
+// } else if (percentage < 40) {
+//   console.log(`your percentage is`, percentage, ` and your grade is F`);
+// } else {
+//   console.log(`please enter a valid marks`);
+// }
+// var bill: any = prompt(`enter your bill units to know your bill`);
+// if (bill <= 50) {
+//   console.log(`your bill is Rs.`, bill * 0.5 + ((bill * 0.5) / 50) * 100);
+// } else if (bill <= 150) {
+//   console.log(`your bill is Rs.`, bill * 0.75 + ((bill * 0.75) / 150) * 100);
+// } else if (bill <= 250) {
+//   console.log(`your bill is Rs.`, bill * 0.75 + ((bill * 0.75) / 150) * 100);
+// }
+// var salary: number = prompt(`enter your basic salary`);
+// var hra: number = (salary / 10000) * 20 + salary;
+// var da: number = (salary / 10000) * 80 + salary;
+// var hRa: number = (salary / 20000) * 25 + salary;
+// var dA: number = (salary / 20000) * 90 + salary;
+// var HRa: number = (salary / salary) * 25 + salary;
+// var DA: number = (salary / salary) * 90 + salary;
+// if (salary <= 10000) {
+//   console.log(`gross salary of HRA =`, hra, `gross salary of Da =`, da);
+// } else if (salary <= 20000) {
+//   console.log(`gross salary of HRA =`, hRa, `gross salry of DA`, dA);
+// } else if (salary > 20000) {
+//   console.log(`gross salary of HRA =`, HRa, `gross salary of HRA =`, DA);
+// }
+// var salary: any = prompt("Enter your basic salary");
+// var newsalary: any = parseInt(salary);
+// var hra: any = (newsalary / 10000) * 20 + newsalary;
+// var da = (newsalary / 10000) * +newsalary;
+// var hRa = (newsalary / 20000) * 25 + newsalary;
+// var dA = (newsalary / 20000) * 90 + newsalary;
+// var HRa = (newsalary / newsalary) * 30 + newsalary;
+// var DA = (newsalary / newsalary) * 95 + newsalary;
+// var salary: any = prompt("Enter your basic salary");
+// var newsalary: any = parseInt(salary);
+// var hra: any = 0.2 * newsalary;
+// var da: any = 0.8 * newsalary;
+// var hRa: any = 0.25 * newsalary;
+// var dA: any = 0.9 * newsalary;
+// var HRa: any = 0.3 * newsalary;
+// var DA: any = 0.95 * newsalary;
+// if (salary <= 10000) {
+//   console.log("Gross salary of person =", hra + da + newsalary);
+// } else if (salary <= 20000) {
+//   console.log("Gross salary of person =", hRa + dA + newsalary);
+// } else if (salary > 20000) {
+//   console.log("Gross salary of person =", HRa + DA + newsalary);
+// } else {
+//   console.log(`please enter a number`);
+// }
