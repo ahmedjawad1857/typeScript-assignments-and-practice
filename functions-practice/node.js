5cae01d2be9780e0023448d9cad7bda5f473a93e
Ahmad Jawad <134083952+ahmedjawad1857@users.noreply.github.com>
2024-06-06 18:13:21 +0500

    created: function-practice

---
+++ functions-practice/node.js
@@ -0,0 +1,82 @@
+"use strict";
+// function table() {
+//   console.log(`Table of 5`);
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(`5 x ${i} = ${5 * i}`);
+//   }
+// }
+// table();
+// function addValues(): number {
+//   var b: number = 7 + 8;
+//   return b;
+// }
+// console.log(addValues());
+// function multiplytwoValues(): number {
+//   var b: number = 7 * 8;
+//   return b;
+// }
+// console.log(multiplytwoValues());
+// function concattostring(): string {
+//   var b: string = `hello `;
+//   var c: string = `world`;
+//   return b.concat(c);
+// }
+// console.log(concattostring());
+// function sliceofstring(): string {
+//   var b: string = `hello world`;
+//   return b.slice(1, 6);
+// }
+// console.log(sliceofstring());
+// function ifelsefunction() {
+//   if (8 >= 9 && 8 >= 12) {
+//     console.log(`8 is max number`);
+//   } else if (12 >= 9 && 12 >= 8) {
+//     console.log(`12 is max number`);
+//   } else {
+//     console.log(`9 is max number`);
+//   }
+// }
+// ifelsefunction();
+// function arrayfunction(): string[] {
+//   var colour: string[] = [`yellow`, `blue`, `red`, `orange`, `ashgrey`];
+//   colour.push(`string`);
+//   return colour;
+// }
+// console.log(arrayfunction());
+// function fortimepass(p1: number, p2: number) {
+//   console.log(p1 * p2);
+//   return p1 + p2;
+// }
+// console.log(fortimepass(10, 5));
+// function table(tablenum: any) {
+//   console.log(`Table of ${tablenum}`);
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(`${tablenum} x ${i} = ${tablenum * i}`);
+//   }
+// }
+// table(prompt(`enter a number which number table you want to know`));
+// function studentData(Name: any, Class: any, Roll_no: any, Age?: any): number {
+//   var studentdata: any = {
+//     Fullname: Name,
+//     Class: Class,
+//     Roll_no: Roll_no,
+//     Age: Age,
+//   };
+//   return studentdata;
+// }
+// var data: any = studentData(
+//   prompt(`Enter Your Fullname`),
+//   prompt(`Enter Your Class`),
+//   prompt(`Enter Your Roll No.`),
+//   prompt(`Enter Your Age`)
+// );
+// console.log(data);
+// function infiniteparam(...number: number[]): number {
+//   var sum: number = 0;
+//   for (let i = 0; i < number.length; i++) {
+//     sum += number[i];
+//   }
+//   return sum;
+// }
+// var infinite: number = infiniteparam(8, 239, 2, 1, 6, 7);
+// console.log(infinite);
