a3de0b9978598689fbbd9d0b9b2c3bdc80a5f945
Ahmad Jawad <134083952+ahmedjawad1857@users.noreply.github.com>
2024-06-06 18:10:37 +0500

    created: filter,every,reduce practice

---
+++ filter-every-reduce-practice/index.js
@@ -0,0 +1,24 @@
+"use strict";
+// var evenArray: number[] = [2, 7, 5, 6, 8, 10, 9, 5, 4, 97];
+// var even = evenArray.filter((items) => items % 2 == 0);
+// console.log(`This is an even array`, even);
+// var odd = evenArray.filter((items) => items % 2 !== 0);
+// console.log(`This is an  odd array`, odd);
+// var every = evenArray.every((value, index) => value % 1 == 0);
+// console.log(every);
+// var everypractice = evenArray.every((value, index) => value % 2 == 0);
+// console.log(everypractice);
+// var number: number[] = [0, 3, 3, 3, 3, 2, 3, 5, 7];
+// var numbermultiply = number.forEach((value) => {console.log(value + 34)});
+// var some1 = number.some((value) => value > 18);
+// console.log(some1);
+// var reduce = number.reduce((item, index) => item + index);
+// console.log(reduce);
+// var strings: string[] = ["hello", "bar", "ipsum"];
+// var capitalizedStrings: string[] = strings.map((str) =>
+//   str
+//     .split(" ")
+//     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+//     .join(" ")
+// );
+// console.log(capitalizedStrings);
