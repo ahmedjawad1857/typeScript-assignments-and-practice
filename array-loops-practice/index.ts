8612e0335910c3bf9974459346f8cca795691581
Ahmad Jawad <134083952+ahmedjawad1857@users.noreply.github.com>
2024-06-06 16:23:44 +0500

    Created: Array iteration Practice

---
+++ array-loops-practice/index.ts
@@ -0,0 +1,42 @@
+ var city: string[] = [`Faisalabad`, `Lahore`, `Karachi`, `Islamabad`];
+var b:string[][] = city.map((arrayItem, index, array) => {
+  return array;
+});
+console.log(b);
+var nunbers: number[] = [1, 4, 73, 55, 6, 7];
+var c = nunbers.map((item, i, Array) => item + 7);
+console.log(c);
+var b2: number[] = [17, 9, 5, 6, 8, 4, 3];
+var d = b2.map((a, i, c) => {
+  if (a % 2 == 0) {
+    return a;
+  }
+});
+console.log(d);
+var r: number[] = [17, 9, 5, 6, 8, 4, 3];
+var e = r.map((item, i, c) => {
+  if (item % 2 !== 0) {
+    return item;
+  }
+});
+console.log(e);
+var b2: number[] = [17, 9, 5, 6, 8, 4, 3];
+var d = b2.map((a, i, c) => {
+  if (i % 2 == 0) {
+    return i;
+  }
+});
+console.log(d);
+var r: number[] = [17, 9, 5, 6, 8, 4, 3];
+var e = r.map((item, i, c) => {
+  if (i % 2 !== 0) {
+    return i;
+  }
+});
+console.log(e);
+var GreetedWords: string[] = [`hello`, `hi`, `How are you`];
+var hello = GreetedWords.map((ahmad, sheraz, shoaib) => {
+  return sheraz;
+});
+console.log(hello);
+console.log();
